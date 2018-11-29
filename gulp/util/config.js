97f22e53ec9215ed5e694e67754bf1e48d9e47b5
Lorin Muller <lorinmuller@Lorins-MacBook-Air.local>
2018-11-29 23:49:30 +0200

    This its my first save in git which its my forst commit

---
+++ gulp/util/config.js
@@ -0,0 +1,10 @@
+const config = {
+  fileExpression: {
+    copy: '**/*.{doc,docx,zip,pdf,js}',
+    font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
+    image: '**/*.{jpg,jpeg,gif,svg,png}',
+    ico: './*.{ico,json}'
+  }
+};
+
+export default config;
